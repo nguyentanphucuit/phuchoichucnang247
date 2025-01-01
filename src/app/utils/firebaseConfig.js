1dc9a02a73294dd10f48c77ede5dd807a531db9b
KID <nguyentanphucuit@gmail.com>
2025-01-01 16:38:04 +0700

    update firebase config

--- src/app/utils/firebaseConfig.js
+++ src/app/utils/firebaseConfig.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBzz7PNjPtA68NJayH46Z36jhXkQ7M4fEo",
+  authDomain: "phcn-5e328.firebaseapp.com",
+  projectId: "phcn-5e328",
+  storageBucket: "phcn-5e328.firebasestorage.app",
+  messagingSenderId: "416504557417",
+  appId: "1:416504557417:web:629873681d4cd9472ad7f8",
+  measurementId: "G-X32XBVKXKJ",
 };
 
 const app = initializeApp(firebaseConfig);
